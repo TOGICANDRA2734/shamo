bed6f51cde09e8b4508af9fffa07495ec1116180
Candra Togi <togicandra@gmail.com>
2023-09-01 20:33:51 +0800

    chat_tile added

--- lib/widgets/chat_tile.dart
+++ lib/widgets/chat_tile.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:shamo/theme.dart';
 
 class ChatTile extends StatelessWidget {
+  const ChatTile({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -11,12 +13,43 @@ class ChatTile extends StatelessWidget {
             Row(
               children: [
                 Image.asset(
-                  "assets/icon_splash.png",
+                  "assets/shop_icon.png",
                   width: 54,
                   height: 54,
+                ),
+                const SizedBox(
+                  width: 12,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Shoe Store",
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 15,
+                        ),
+                      ),
+                      Text(
+                        "Good night, This item is on...",
+                        style: secondaryTextStyle.copyWith(
+                          fontSize: 14,
+                          fontWeight: light,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      )
+                    ],
+                  ),
+                ),
+                Text(
+                  "now",
+                  style: secondaryTextStyle.copyWith(
+                    fontSize: 14,
+                  ),
                 )
               ],
-            )
+            ),
+            // SizedBox(12,1);
           ],
         ));
   }
